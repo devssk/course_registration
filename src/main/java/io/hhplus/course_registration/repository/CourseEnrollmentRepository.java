ae0af20f524f1e09f102dad903344ebc9df6af5d
devssk <sksong419@naver.com>
2024-10-03 21:57:01 +0900

    feat: 수강 인원 조회에 대해 비관적 락 적용

--- src/main/java/io/hhplus/course_registration/repository/CourseEnrollmentRepository.java
+++ src/main/java/io/hhplus/course_registration/repository/CourseEnrollmentRepository.java
@@ -1,11 +1,15 @@
 package io.hhplus.course_registration.repository;
 
 import io.hhplus.course_registration.entity.CourseEnrollment;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 
 public interface CourseEnrollmentRepository extends JpaRepository<CourseEnrollment, Long> {
     List<CourseEnrollment> findAllByCourseInfoCourseInfoIdIn(List<Long> courseInfoIdList);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     CourseEnrollment findByCourseInfoCourseInfoId(Long courseInfoId);
 }
